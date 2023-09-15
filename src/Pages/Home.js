f077a6b5b19b05428c8de3bcaacf16065fe212dd
phgnguyndo <nguyenphuongdo22@gmail.com>
2023-09-15 22:21:17 +0700

    Create1

---
+++ src/Pages/Home.js
@@ -0,0 +1,10 @@
+import { Button, Link } from "@chakra-ui/react";
+import 'bootstrap/dist/css/bootstrap.css';
+const Home = () => {
+  return (
+    <>
+      <Link href="/product">Click me</Link>
+    </>
+  );
+};
+export default Home;
---
+++ src/Pages/Product.js
@@ -0,0 +1,10 @@
+import { Button, Link } from "@chakra-ui/react";
+import 'bootstrap/dist/css/bootstrap.css';
+const Product = () => {
+  return (
+    <>
+      xin chao
+    </>
+  );
+};
+export default Product;
